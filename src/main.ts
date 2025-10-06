7d7bfe6652fadbffaa25152fc28aef1459847454
ghts2110 <gabrieltorres2110@gmail.com>
2025-10-06 14:50:39 -0300

    list logs

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,32 @@
-console.log("Hello, World!");
+import { promises as fs } from "node:fs";
+import path from "node:path";
+
+const logsDir = path.resolve(__dirname, "..", "logs");
+
+type Dirent = import("fs").Dirent;
+
+async function getAllLogFiles(dir: string): Promise<string[]> {
+	const entries: Dirent[] = await fs.readdir(dir, { withFileTypes: true });
+
+	const files = await Promise.all(
+        entries
+        .filter((entry) => !entry.name.startsWith('.')) 
+        .map(async (entry) => {
+            const fullPath = path.join(dir, entry.name);
+
+            if (entry.isDirectory()) {
+            return getAllLogFiles(fullPath);
+            } else {
+            return [fullPath];
+            }
+        })
+    );
+
+	return files.flat();
+}
+
+(async () => {
+	const allFiles = await getAllLogFiles(logsDir);
+	console.log("Arquivos encontrados:");
+	for (const f of allFiles) console.log(" -", path.basename(f));
+})();
