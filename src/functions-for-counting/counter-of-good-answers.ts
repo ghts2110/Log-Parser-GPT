5d18aa67a08a5cd5c812f04bb22e42f4c178b0fb
ghts2110 <gabrieltorres2110@gmail.com>
2025-10-15 15:15:47 -0300

    collect data of log

---
+++ src/functions-for-counting/counter-of-good-answers.ts
@@ -0,0 +1,76 @@
+import { promises as fs } from "node:fs";
+import path from "node:path";
+import { getAllFiles } from "../file-management";
+
+const outDir  = path.join(__dirname, '../../', 'logs/output');
+
+interface Message {
+    role?: string;
+    content: string;
+}
+
+interface _Data {
+    body?: string;
+}
+
+interface Reply {
+    _data?: _Data;
+}
+
+interface LogEntry {
+    messages?: Message[];
+    text?: string;
+    reply?: Reply;
+}
+
+function collectData(content: LogEntry[]){
+    let context: string = "";
+    let question: string = "";
+    let answer: string = "";
+
+    for(const entry of content){
+        if(entry.messages){
+            for(const msg of entry.messages){
+                if(msg.role === "system"){
+                    context += msg.content;
+                }
+
+                if(msg.role === "user"){
+                    question = msg.content;
+                }
+            }
+        }
+
+        if(entry.reply){
+            if(entry.reply._data){
+                if(entry.reply._data.body) {
+                    answer = entry.reply._data.body
+                }
+            }
+        }
+    }
+
+    // passar para o LLM verificar
+}
+
+async function countGood(){
+    const allFiles = await getAllFiles(outDir);
+    
+    if (allFiles.length === 0) {
+        console.log('Nenhum arquivo encontrado em logs/input/.');
+        return;
+    }
+    
+    for (const f of allFiles) {
+        const content = JSON.parse(await fs.readFile(f, 'utf8'))
+
+        collectData(content)
+    }
+
+    // printar o resultado
+}
+
+countGood().catch((err) => {
+  console.error("Erro no main:", err);
+  process.exit(1);
+});
